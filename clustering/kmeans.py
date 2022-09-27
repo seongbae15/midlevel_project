6711d3f5748f2371c6531161f06e0a2ca375d8d2
Jieun Park <diwoon95@gmail.com>
2022-09-27 16:05:55 +0900

    k-means data add

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -13,18 +13,18 @@ if not img_files: # if empty folder
     print('there are no jpg files')
     sys.exit()
 
-''' check of printed path 
+''' # check of printed path 
 for f, g, h in img_files:
+    print(f,g,h)
     print(f+'/'+h[0])
 '''
 
-
-for f, g, h  in img_files:
+for i, f in enumerate(img_files[0][2]):
     # t.ly/zgLP
-    img = cv2.imread(f+'/'+h[0])
+    img = cv2.imread(file_path+'/'+f)
     vectorized = img.reshape((-1,3))
     vectorized = np.float32(vectorized)
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 1.0)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 0.5)
     K = 5 # N of colors
     attempts=10
     ret,label,center=cv2.kmeans(vectorized,K,None,criteria,attempts,cv2.KMEANS_PP_CENTERS)
@@ -32,7 +32,7 @@ for f, g, h  in img_files:
     
     res = center[label.flatten()]
     result_image = res.reshape((img.shape))
-    cv2.imwrite(f'./image_result/{h[0]}_cluster.jpg', result_image)
+    cv2.imwrite(f'./image_result/{f}_cluster.jpg', result_image)
 
     cv2.imshow('Original Image',img)
     cv2.imshow('Segmented Image when K = %i' % K, result_image)
