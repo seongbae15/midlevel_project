73d6f3016aa2c03766800c33ab3c4c0d93914fed
Jieun Park <diwoon95@gmail.com>
2022-10-06 20:34:31 +0900

    deepgaze running with iteration

--- deepgaze_sample/run_dg2.py
+++ deepgaze_sample/run_dg2.py
@@ -1,6 +1,7 @@
 #%%
 from logging import root
 import os
+import sys
 from cmath import log
 from hmac import trans_36
 import numpy as np
@@ -11,69 +12,74 @@ from scipy.special import logsumexp
 import torch
 import torchvision
 from torchvision import transforms
+from torch.utils.data import DataLoader
 from torchvision.transforms.functional import to_pil_image
-
+import warnings
 import deepgaze_pytorch
 
+warnings.filterwarnings("ignore")
 DEVICE = "cpu"
 
 
 # image = face() #racoon face image load
-train_path = "../data_sample/"
+train_path = "../data_sample"  # on notebook
 
 
 # you can use DeepGazeI or DeepGazeIIE
-if train_path is None:
-    
+
+
 model = deepgaze_pytorch.DeepGazeIIE(pretrained=True).to(DEVICE)
 # image resize
 resize_trans = transforms.Compose(
     [transforms.Resize((800, 1024)), transforms.ToTensor()]
 )
-resize_train = torchvision.datasets.ImageFolder(
-    root=train_path, transform=resize_trans
-)
-image = resize_train[0][0]
-print("loaded shape:", image.shape)
 
-image_unsq = image.unsqueeze(dim=0)
-print("unsq shape:", image_unsq.shape, type(image_unsq))
-# load precomputed centerbias log density (from MIT1003) over a 1024x1024 image
-# you can download the centerbias from https://github.com/matthias-k/DeepGaze/releases/download/v1.0.0/centerbias_mit1003.npy
-# alternatively, you can use a uniform centerbias via `centerbias_template = np.zeros((1024, 1024))`.
+# with folder:
 
-# centerbias_template = np.load('centerbias_mit1003.npy')
-centerbias_template = np.zeros((800, 1024))
-
-# rescale to match image size
-centerbias = zoom(
-    centerbias_template,
-    (
-        image.shape[1] / centerbias_template.shape[0],
-        image.shape[2] / centerbias_template.shape[1],
-    ),
-    order=0,
-    mode="nearest",
-)
-# renormalize log density
-centerbias -= logsumexp(centerbias)
-centerbias_tensor = torch.tensor([centerbias]).to(DEVICE)
-
-log_density_prediction = model(image_unsq, centerbias_tensor)
-print(log_density_prediction.shape)
-f, axs = plt.subplots(nrows=3, ncols=1, figsize=(8, 6))
-axs[0].imshow(torch.transpose(image, 2, 0).transpose(0, 1))
-axs[1].matshow(
-    np.exp(log_density_prediction.detach().cpu().numpy()[0, 0]),
-    alpha=0.5,
-    cmap=plt.cm.RdBu,
-)
-axs[1].imshow(torch.transpose(image, 2, 0).transpose(0, 1), alpha=0.4)
-axs[1].axis("off")
-axs[2].matshow(
-    np.exp(log_density_prediction.detach().cpu().numpy()[0, 0]), cmap=plt.cm.RdBu
-)
-axs[2].axis("off")
+dataloader = torchvision.datasets.ImageFolder(root=train_path, transform=resize_trans)
 
 
+for pics, idx in iter(dataloader):
+    print(len(pics), idx)
+    """
+    image = pics
+    image_unsq = image.unsqueeze(dim=0)
+    # load precomputed centerbias log density (from MIT1003) over a 1024x1024 image
+    # you can download the centerbias from https://github.com/matthias-k/DeepGaze/releases/download/v1.0.0/centerbias_mit1003.npy
+    # alternatively, you can use a uniform centerbias via `centerbias_template = np.zeros((1024, 1024))`.
+
+    # centerbias_template = np.load('centerbias_mit1003.npy')
+    centerbias_template = np.zeros((800, 1024))
+
+    # rescale to match image size
+    centerbias = zoom(
+        centerbias_template,
+        (
+            image.shape[1] / centerbias_template.shape[0],
+            image.shape[2] / centerbias_template.shape[1],
+        ),
+        order=0,
+        mode="constant", # n
+    )
+    # renormalize log density
+    centerbias -= logsumexp(centerbias)
+    centerbias_tensor = torch.tensor([centerbias]).to(DEVICE)
+
+    log_density_prediction = model(image_unsq, centerbias_tensor)
+    print(log_density_prediction.shape)
+    f, axs = plt.subplots(nrows=1, ncols=3, figsize=(12, 18))
+    axs[0].imshow(torch.transpose(image, 2, 0).transpose(0, 1))
+    axs[1].matshow(
+        np.exp(log_density_prediction.detach().cpu().numpy()[0, 0]),
+        alpha=0.5,
+        cmap=plt.cm.RdBu,
+    )
+    axs[1].imshow(torch.transpose(image, 2, 0).transpose(0, 1), alpha=0.4)
+    axs[1].axis("off")
+    axs[2].matshow(
+        np.exp(log_density_prediction.detach().cpu().numpy()[0, 0]), cmap=plt.cm.RdBu
+    )
+    axs[2].axis("off")
+    plt.savefig(f'../data_result/{idx}_reuslt.png')
+    """
 # %%
