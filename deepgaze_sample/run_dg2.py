859bdc94f1113ea85b0d6b98f93836122459bc78
Jieun Park <diwoon95@gmail.com>
2022-10-10 18:03:22 +0900

    deepgaze tuning trial - until 8

--- deepgaze_sample/run_dg2.py
+++ deepgaze_sample/run_dg2.py
@@ -5,7 +5,6 @@ from cmath import log
 from hmac import trans_36
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.misc import face
 from scipy.ndimage import zoom
 from scipy.special import logsumexp
 import torch
@@ -24,9 +23,51 @@ train_path = "../data_sample"  # on notebook
 
 
 # you can use DeepGazeI or DeepGazeIIE
-
-
 model = deepgaze_pytorch.DeepGazeIIE(pretrained=True).to(DEVICE)
+# model config
+model.BACKBONES = [
+    {
+        "type": "deepgaze_pytorch.features.shapenet.RGBShapeNetC",
+        "used_features": [
+            "1.module.layer3.0.conv2",
+            "1.module.layer3.3.conv2",
+            "1.module.layer3.5.conv1",
+            "1.module.layer3.5.conv2",
+            "1.module.layer4.1.conv2",
+            "1.module.layer4.2.conv2",
+        ],
+        "channels": 2048,
+    },
+    {
+        "type": "deepgaze_pytorch.features.efficientnet.RGBEfficientNetB5",
+        "used_features": [
+            "1._blocks.24._depthwise_conv",
+            "1._blocks.26._depthwise_conv",
+            "1._blocks.35._project_conv",
+        ],
+        "channels": 2416,
+    },
+    {
+        "type": "deepgaze_pytorch.features.densenet.RGBDenseNet201",
+        "used_features": [
+            "1.features.denseblock4.denselayer32.norm1",
+            "1.features.denseblock4.denselayer32.conv1",
+            "1.features.denseblock4.denselayer31.conv2",
+        ],
+        "channels": 2048,
+    },
+    {
+        "type": "deepgaze_pytorch.features.resnext.RGBResNext50",
+        "used_features": [
+            "1.layer3.5.conv1",
+            "1.layer3.5.conv2",
+            "1.layer3.4.conv2",
+            "1.layer4.2.conv2",
+        ],
+        "channels": 2560,
+    },
+]
+
 # image preprocess
 
 # image resize
@@ -44,7 +85,6 @@ for idx, pics in enumerate(dataloader):
 
     image = color.rgb2lab(rgb=image_rgb.T, channel_axis=-1)  # color space to lab
     # result = 800, 1200, 3 -> 바꿔줘야
-
     image = torch.tensor(image.transpose(2, 1, 0)).to(DEVICE)
 
     image_unsq = image.unsqueeze(dim=0)
@@ -54,7 +94,7 @@ for idx, pics in enumerate(dataloader):
     # alternatively, you can use a uniform centerbias via `centerbias_template = np.zeros((1024, 1024))`.
 
     centerbias_template = np.load("centerbias_mit1003.npy")
-    # centerbias_template = np.zeros((800, 800))
+    # centerbias_template = np.zeros((1024, 1024))
 
     # rescale to match image size
     centerbias = zoom(
@@ -71,8 +111,9 @@ for idx, pics in enumerate(dataloader):
     centerbias_tensor = torch.tensor([centerbias]).to(DEVICE)
 
     log_density_prediction = model(image_unsq, centerbias_tensor)
-    print(log_density_prediction.shape)
-    f, axs = plt.subplots(nrows=1, ncols=3, figsize=(12, 18))
+    if idx % 10 == 0:
+        print(f"number {idx} image is processed")
+    f, axs = plt.subplots(nrows=1, ncols=3, figsize=(18, 12))
     axs[0].imshow(torch.transpose(image_rgb, 2, 0).transpose(0, 1))
     axs[1].matshow(
         np.exp(log_density_prediction.detach().cpu().numpy()[0, 0]),
